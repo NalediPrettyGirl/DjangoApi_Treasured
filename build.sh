c15e6d1ab556063702101e94fd979fd91542df75
Naledi Motsoikha <116162583+NalediPrettyGirl@users.noreply.github.com>
2026-08-09 17:08:34 +0200

    Modify migration command in build script
    Fake migration to resolve 'already exists' error on Render.

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+# Fake migration 0004 to fix the "already exists" error on Render
+python manage.py migrate store_api 0004 --fake || true
 python manage.py migrate
