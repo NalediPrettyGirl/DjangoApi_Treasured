8fa7af1caf57d50b62ef3eef49a284368ec71aa8
Naledi Motsoikha <116162583+NalediPrettyGirl@users.noreply.github.com>
2026-08-09 17:10:44 +0200

    Update migration commands in build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-# Fake migration 0004 to fix the "already exists" error on Render
-python manage.py migrate store_api 0004 --fake || true
+# Fake all migrations for store_api to sync the DB state
+python manage.py migrate store_api --fake || true
 python manage.py migrate
