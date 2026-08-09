d3030850285abbff792bc8f396b7a740327e5c64
Naledi Motsoikha <116162583+NalediPrettyGirl@users.noreply.github.com>
2026-08-10 01:52:17 +0200

    Remove fake migration for store_api in build script

--- build.sh
+++ build.sh
@@ -4,6 +4,4 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-# Fake all migrations for store_api to sync the DB state
-python manage.py migrate store_api --fake || true
 python manage.py migrate
