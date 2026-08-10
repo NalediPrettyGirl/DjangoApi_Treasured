2ce7343f05ba5d91ec9a106e495842092708f421
Naledi Motsoikha <116162583+NalediPrettyGirl@users.noreply.github.com>
2026-08-10 02:07:26 +0200

    Update build.sh to include fake migration step
    Add fake migration for store_api to skip existing columns

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+
+# Fake up to 0005 (because your DB already has those columns), then apply the rest normally!
+python manage.py migrate store_api 0005 --fake || true
 python manage.py migrate
